26837c1b41fa375c266f2a340e964f8263f848c7
Karl Hohn <kfhohn@gmail.com>
2020-01-31 14:55:02 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// alert('Hello world');
+// console.log('hello world');
+
+function swapColorClasses(event) {
+  event.preventDefault();
+
+  console.log(event.target);
+
+  var demoModule = document.querySelector('.demo-module');
+
+  if (demoModule.classList.contains('blue')) {
+    demoModule.classList.remove('blue');
+    demoModule.classList.add('green');
+  } else if (demoModule.classList.contains('green')) {
+    demoModule.classList.remove('green');
+    demoModule.classList.add('purple');
+  } else if (demoModule.classList.contains('purple')) {
+    demoModule.classList.remove('purple');
+    demoModule.classList.add('blue');
+  } else {
+    demoModule.classList.add('blue');
+  }
+}
+
+// function toggleDifferentDivs(event) {
+//   event.preventDefault();
+//
+//   demoModule.addClass('hidden');
+//
+//   console.log($(event.target).attr('href'));
+//
+//   $($(event.target).attr('href')).removeClass('hidden');
+// }
+//
+// $('#firstClick').click(toggleDifferentDivs);
+// $('#secondClick').click(toggleDifferentDivs);
+
+
+// $('#clickMe').click(swapColorClasses);
+// $('#orMe').click(swapColorClasses);
+
+document.getElementById('clickMe').addEventListener('click', swapColorClasses);
+document.getElementById('orMe').addEventListener('click', swapColorClasses);
